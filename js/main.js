d5946c16df067074e2f483fc6c2fc6213739d8fc
Antonia Cerda <antoniacerdag@gmail.com>
2017-05-12 11:25:24 -0300

    haciendo las validaciones

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 var clientes = document.getElementById("clientes");
 var productos = document.getElementById("productos");
 function mostrarData(){
@@ -9,17 +7,33 @@ function mostrarData(){
 	var direccion = document.getElementById("email").value;
 	var telefono = document.getElementById("telefono").value;
 
-	if(nombre.length == 0){
-		return ("Complete el campo Nombre");
-	} if (apellido.length == 0) {
-		return("Complete el campo Apellido");
-	} if (email.length == 0) {
-		return("Complete el campo Email");
-	} if (direccion.length == 0) {
-		return("Complete el campo Dirección");
-	}if (telefono) {}
+	
+	//var value1 = document.getElementsByTagName("option")[0].value
+	//var value2 = document.getElementsByTagName("option")[1].value
+	//var value3 = document.getElementsByTagName("option")[2].value
+	//var value4 =document.getElementsByTagName("option")[3].value
+	var checkbox = document.getElementById("checkbox");
 
 	var infoclientes = ("<b> Nombre : </b>" + nombre + "<br>" + "<b> Apellido : </b>" + apellido + "<br>" + "<b> Email : </b>" + 
-		email + "<br>" + "<b> Dirección : </b>" + direccion + "<br>" + "<b> Teléfono : </b>" + telefono);
-	clientes.innerHTML = (infoclientes);
+	email + "<br>" + "<b> Dirección : </b>" + direccion + "<br>" + "<b> Teléfono : </b>" + telefono);
+	
+
+
+	if((nombre.length == 0) || (apellido.length == 0) || (email.length == 0) || (direccion.length == 0) || (telefono.length == 0) ){
+		alert ("Complete todos los campos");
+	} else{
+		clientes.innerHTML = (infoclientes);
+	}
+	
+	
+
+	
+}
+
+function opciones(){
+	var select=document.getElementsByTagName("select");
+	for (var i=0; i<select.length; i++){
+        if(select[i].value=="0"){
+            alert("Debe elegir opción");}
+	}
 }
